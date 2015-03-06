9a5ed3554530c4b33b4ff0d3ce5a1b664888dcf6
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-06 17:17:42 +0100

    + Create controller for event resource.

--- js/app/app.js
+++ js/app/app.js
@@ -1,3 +1,20 @@
-var app = angular.module('calendar', [
+(function () {
+  var app = angular.module('calendar', [
 
   ]);
+
+  app.controller('EventController', ['$scope', function ($scope) {
+    this.events = [
+      {
+        "title": "Facilitate Discrete Math meeting.",
+        "date": 8
+      },
+      {
+        "title": "Take Python class exam.",
+        "date": 7
+      }
+    ];
+
+    this.heading = "Calendar";
+  }] );
+}());
