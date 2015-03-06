96b85367d4869de889d1c4bdea8c5d2e37fcf2ba
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-06 18:02:39 +0100

    + Add edit and delete buttons to each day on the calendar. + Improve basic design.

--- js/app/app.js
+++ js/app/app.js
@@ -6,13 +6,44 @@
   app.controller('EventController', ['$scope', function ($scope) {
     this.events = [
       {
-        "title": "Facilitate Discrete Math meeting.",
-        "date": 8
+        "title": "",
+        "day": 1
       },
+
       {
-        "title": "Take Python class exam.",
-        "date": 7
-      }
+        "title": "",
+        "day": 2
+      },
+
+      {
+        "title": "",
+        "day": 3
+      },
+
+      {
+        "title": "",
+        "day": 4
+      },
+
+      {
+        "title": "",
+        "day": 5
+      },
+
+      {
+        "title": "Grade Udacity project.",
+        "day": 6
+      },
+
+      {
+        "title": "Take Edx Python class exam.",
+        "day": 7
+      },
+
+      {
+        "title": "Facilitate Discrete Math class",
+        "day": 8
+      },
     ];
 
     this.heading = "Calendar";
