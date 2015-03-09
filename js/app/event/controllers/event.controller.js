db2bf55ac22362ad5eb20bf57b5a5785788c920a
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-09 05:49:04 +0100

    + Refactor EventService to read data from JSON file. + Add form for creating new events to the calendar view. + Implement event addition functionality in EventController. + Add collection of events for a specific day to the calendar view. + Add buttons for editing and deleting events to the view for each event in a specific day. + Implement event deletion functionality in EventController. + Implement event editing functionality in EventController.

--- js/app/event/controllers/event.controller.js
+++ js/app/event/controllers/event.controller.js
@@ -3,10 +3,48 @@
 angular.module('event')
   .controller('EventController', ['EventService', function (EventService) {
     var register = this;
+    register.events = [];
+    register.current = 0;
+
+    register.eventID = null;
+    register.dayID = null;
+
+    register.adding = true;
+    register.editing = false;
+
     EventService.getEvents().success(function (data) {
-      register.events = data;
+      register.events = data.days;
     })
     .error(function (err) {
       console.log(err);
     });
+
+    register.addEvent = function (dayID, eventData) {
+      var newEvent = {};
+      newEvent.title = eventData.title;
+
+      register.events[dayID - 1].events.push(newEvent);
+      register.newEvent = {};
+    };
+
+    register.prepareEdit = function (dayID, eventData, eventID) {
+      register.editing = true;
+      register.adding = false;
+      register.newEvent = eventData;
+      register.eventID = eventID;
+      register.dayID = dayID;
+    };
+
+    register.editEvent = function () {
+      var editedEvent = {};
+      editedEvent.title = register.newEvent.title;
+      register.events[register.dayID - 1].events[register.eventID] = editedEvent;
+      register.editing = false;
+      register.adding = true;
+      register.newEvent = {};
+    }
+
+    register.removeEvent = function (dayID, eventID) {
+      register.events[dayID - 1].events.splice(eventID, 1);
+    };
   }]);
