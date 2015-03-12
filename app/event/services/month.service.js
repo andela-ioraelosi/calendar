751f972c4d925f3c94c58d1cd88ae4ecce6754ae
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-12 21:47:15 +0100

    + Add method to fetch id of the first day.

--- app/event/services/month.service.js
+++ app/event/services/month.service.js
@@ -1,22 +1,45 @@
 angular.module('event')
   .factory('MonthService', ['$http', '$q', function ($http, $q) {
 
-  var service = {};
   var months = [];
-  service.getMonths = function () {
-    var monthsDefer = $q.defer();
-    if (months.length > 0) {
-      monthsDefer.resolve(months);
-    } else {
-      $http.get('./assets/data/events.json').success(function (data) {
-        months = data;
-        monthsDefer.resolve(data);
-      });
+
+  return {
+    getMonths: function () {
+      if (months.length === 0) {
+        $http.get('./assets/data/events.json')
+          .success(function (response) {
+            for (var i = 0, dataLength = response.length; i < dataLength; i++) {
+              months.push(response[i]);
+            }
+          });
+      }
+
+      return months;
+    },
+
+    addEvent: function (monthID, dayID, eventData) {
+
+      months[monthID].days[dayID].events.push(eventData);
+    },
+
+    saveEvent: function (monthID, dayID, eventID, eventData) {
+
+      months[monthID].days[dayID].events[eventID] = eventData;
+    },
+
+    removeEvent: function (monthID, dayID, eventID) {
+
+      months[monthID].days[dayID].events.splice(eventID, 1);
+    },
+
+    getFirstDayID: function (monthID) {
+      var days = months[monthID].days;
+      for (var i = 0, dayCount = days.length; i < dayCount; i++) {
+        if (days[i].id === 1) {
+          return i;
+        }
+      }
     }
-
-    return monthsDefer.promise;
   };
-
-  return service;
 
   }]);
